f7d4898776c48ca3ba6ac0ca2ea53ef5a8b44825
Shreyask24 <shreyask495@gmail.com>
2023-10-06 20:36:31 +0530

    Create Routes and pages

---
+++ Realtor-clone/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+
+// Components
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import Offers from "./pages/Offers"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import ForgotPassword from "./pages/ForgotPassword"
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
