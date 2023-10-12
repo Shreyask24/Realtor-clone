b08d21635873ac305f42b8ab635bfc395015e9cd
Shreyask24 <shreyask495@gmail.com>
2023-10-13 00:21:18 +0530

    Completed Google Autherization

--- Realtor-clone/src/components/OAuth.jsx
+++ Realtor-clone/src/components/OAuth.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { FcGoogle } from 'react-icons/fc'
+import { toast } from 'react-toastify'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router';
+
+// Components
+import { db } from '../firebase';
 
 const OAuth = () => {
+    const navigate = useNavigate();
+
+    async function onGoogleClick() {
+        try {
+            const auth = getAuth()
+            const provider = new GoogleAuthProvider();
+
+            const result = await signInWithPopup(auth, provider);
+            const user = result.user;
+
+            // Check If The USer Exists
+            const docRef = doc(db, "users", user.uid);
+            const docSnap = await getDoc(docRef);
+
+            if (!docSnap.exists()) {
+                await setDoc(docRef, {
+                    name: user.displayName,
+                    email: user.email,
+                    timestamp: serverTimestamp(),
+                });
+            }
+            navigate("/");
+
+        } catch (error) {
+            toast.error("Could not Authorize with Google");
+            console.log(error)
+        }
+
+
+    }
+
     return (
-        <button className='w-full flex justify-center items-center bg-red-700 text-white px-3 py-7 uppercase rounded text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-sm hover:shadow-md active:shadow-lg transition duration-150 ease-in-out'>
+        <button type='button' onClick={onGoogleClick} className='w-full flex justify-center items-center bg-red-700 text-white px-3 py-7 uppercase rounded text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-sm hover:shadow-md active:shadow-lg transition duration-150 ease-in-out'>
             <FcGoogle className='mr-2 text-2xl bg-white rounded-full' /> Continue With Google
         </button>
     )
